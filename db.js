520992c57598c30564f413469570d19876fb33e1
use-lie-die <tencent.lj@gmail.com>
2025-05-27 00:19:18 +0300

    new pg pool for railways

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: 'postgres',       // твой пользователь
-  host: 'localhost',      // адрес сервера
-  database: 'diplom1',     // имя БД
-  password: '08052003',       // пароль
-  port: 5432,             // порт
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
