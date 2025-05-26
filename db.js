ffa7fd7b29d750268ae0b093e8bacc83c71775d1
Isabella Francesco <114323497+yokonohana@users.noreply.github.com>
2025-05-27 00:41:13 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,12 +2,12 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || '5432'),
-  ssl: { rejectUnauthorized: false }
+  host: process.env.PGHOST,
+  port: parseInt(process.env.PGPORT || '5432'),
+  user: process.env.POSTGRES_USER,
+  password: process.env.PGPASSWORD,
+  database: process.env.POSTGRES_DB,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
