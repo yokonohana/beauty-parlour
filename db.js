602a4a0e63bd305db7803ec5838f898908e1b122
use-lie-die <tencent.lj@gmail.com>
2025-05-27 03:19:54 +0300

    update env 4

--- db.js
+++ db.js
@@ -4,8 +4,11 @@ import 'dotenv/config';
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
 });
 
 export default pool;
