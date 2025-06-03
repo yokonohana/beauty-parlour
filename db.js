2ff5b4a3965dfa4658281b2c8082b5be5614ed25
use-lie-die <tencent.lj@gmail.com>
2025-06-03 18:05:50 +0300

    upd db

--- db.js
+++ db.js
@@ -4,12 +4,11 @@ import 'dotenv/config';
 const { Pool } = pg;
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
-  ssl: process.env.PGSSL === 'true' ? { rejectUnauthorized: false } : false,
+  host: 'centerbeam.proxy.rlwy.net',
+  user: 'postgres',
+  password: 'oSxDAMNNNUMuqvnuqcJXhCIQCHjMBHNe',
+  database: 'railway',
+  port: 34401,
 });
 
 export default pool;
